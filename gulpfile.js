33187abde49b9a676e39d9ac40f4f5e071d13b32
inuscript <inuscript@gmail.com>
2015-07-25 10:27:22 +0900

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp")
+var iconfont = require("gulp-iconfont")
+var sass = require("gulp-sass")
+var jsonSass = require("json-sass")
+
+gulp.task("default", function(){
+  gulp.src(["svg/*.svg"])
+    .pipe(iconfont({
+      fontName: "myFont",
+    }))
+    .on("glyphs", function(glyphs, options){
+      var glp = (function(){
+        var _glp = {}
+        glyphs.forEach(function(glyph){
+          _glp[glyph.name] = glyph.unicode[0].charCodeAt(0).toString(16).toUpperCase()
+        })
+        return _glp
+      })()
+      var vars = jsonSass.convertJs({
+        fontName: "myFont",
+        fontPath: "./dest/fonts/",
+        glyphs: glp
+      })
+      console.log(vars)
+    })
+    .pipe(gulp.dest("./dest/fonts"))
+})
