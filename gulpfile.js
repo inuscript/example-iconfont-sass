1e43809068ed359a637142be01e239944fb545e5
inuscript <inuscript@gmail.com>
2015-07-29 00:01:43 +0900

    to task

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,11 @@
 var del = require("del")
 var gulp = require("gulp")
-var gutil = require("gulp-util")
 var iconfont = require("gulp-iconfont")
 var sass = require("gulp-sass")
-var jsonSass = require("json-sass")
-var quote = require("quote")
+var stream = require("stream")
 var gulpSvgicons2svgfont = require('gulp-svgicons2svgfont')
 var svgicons2svgfont = require('svgicons2svgfont')
-var duplexer = require("plexer")
-
-var transform = function(options){
-  return function(err, buf, cb){
-    if(err) {
-      cb(new gutil.PluginError(PLUGIN_NAME, err, {showStack: true}));
-    }
-    try{
-      buf = new Buffer()
-    }catch(err2) {
-      cb(new gutil.PluginError(PLUGIN_NAME, err2, {showStack: true}));
-    }
-  }
-}
-
-var iconfontSass = function(options){
-  svgicons2svgfont(options)
-  return through2({objectMode: true}, function(file, buf, cb){
-    console.log(arguments)
-    cb()
-  })
-  var inStream = 
-  var outStream = through2({objectMode: true}, function(file, buf, cb){
-    inStream.on('glyphs', function(glyphs, option){
-      var value = iconfontValue(glyphs, options)
-      buf = new Buffer(value)
-      cb(buf)
-      return ;
-      // value.path = quote(fontDestPath)
-      // fakeSrc(value, "var/_fonts.scss")
-      // .pipe(gulp.dest("scss/auto-generated"))
-    })
-  })
-  var duplexStream = duplexer({ objectMode: true }, inStream, outStream)
-  return duplexStream
-}
+var iconfontSass = require("./task")
 
 var fakeSrc = function(value, fileName){
   var scss = "$font: " + jsonSass.convertJs(value) + " !default;"
--- task.js
+++ task.js
@@ -1,12 +1,15 @@
 var stream = require("stream")
 var through2 = require("through2")
 var plexer = require("plexer")
-var gulpSvgicons2svgfont = require('gulp-svgicons2svgfont')
-
+var svgicons2svgfont = require('gulp-svgicons2svgfont')
+var iconfont = require('gulp-iconfont')
+var quote = require("quote")
+var jsonSass = require("json-sass")
+var gutil = require("gulp-util")
 
 var PLUGIN_NAME = "iconfont-sass"
 
-var iconfontValue = function(glyphs, options){
+var glyphsMap = function(glyphs){
   var glp = (function(){
     var _glp = {}
     glyphs.forEach(function(glyph){
@@ -15,14 +18,26 @@ var iconfontValue = function(glyphs, options){
     return _glp
   })()
   return {
-    fontName: options.fontName,
     glyphs: glp
   }
 }
 
-module.exports = function(opt){
-  var inputStream = new Stream.Transform({ objectMode: true });
-  var outputStream = new Stream.PassThrough({ objectMode: true });
-  var stream = plexer({ objectMode: true }, inputStream, outputStream);
-
+module.exports = function(options){
+  var inputStream = svgicons2svgfont(options)
+  // var inputStream = iconfont(options)
+  var outputStream = new stream.PassThrough({ objectMode: true });
+  var _glyphs = undefined;
+  var _opt = undefined;
+  inputStream.on('glyphs', function(glyphs, options){
+    _glyphs = glyphs // memorize
+    _opt = options
+  }).pipe(through2.obj(function(file, enc, cb){
+    console.log(arguments)
+    var value = glyphsMap(_glyphs)
+    var scss = "$font: " + jsonSass.convertJs(value) + " !default;"
+    outputStream.push(new gutil.File({ cwd: "", base: "", path: "aaa.scss", contents: new Buffer(scss) }))
+    outputStream.end();
+  }))
+  var duplexStream = plexer({ objectMode: true }, inputStream, outputStream)
+  return duplexStream
 }
